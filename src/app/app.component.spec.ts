3f3a6f2146311deeef8bf5dcd51e557533b6af03
Ruslan Salakhov <ruslan.salakhov@simbirsoft.com>
2021-07-29 23:08:56 +0300

    Добавил PWA

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'terraforming-mars'`, () => {
+  it(`should have as title 'board-games-counter'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('terraforming-mars');
+    expect(app.title).toEqual('board-games-counter');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('terraforming-mars app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'board-games-counter app is running!'
+    );
   });
 });
