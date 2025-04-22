5170593240e21812ce041b202e97fc06880546f2
Ruslan Salakhov <salrusrad@yandex.ru>
2025-04-22 20:55:31 +0300

    Правка иконок

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-import { ThemeService } from './services/theme.service';
+import { Component } from "@angular/core";
+import { ThemeService } from "./services/theme.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
   isDark = this.themeService.isDark;
